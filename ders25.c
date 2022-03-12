d9948433798296a5aef69e0423d3d98be7d444ff
hsnacar <hsncr@yandex.com>
2022-03-12 13:41:34 +0300

    ...

--- ders25.c
+++ ders25.c
@@ -32,7 +32,7 @@ Kodumuzu inceleyip olan bitene bakalım:
 5. satırda beş elemanlı int türünde bir dizi tanımladık.
 6. satırda int* diziAdres ile bir pointer değişken tanımladık.
 8. satırda diziAdres pointer değişkenine dizi değişkeninin 3 nolu indeksindeki verinin adresini atıyoruz. *diziAdres diye ekrana basarsak dizi[3]'ü ekrana basmış oluruz.
-11. satırda diziAdres++ yapıldığında adres numarasında 1 değil 4 artış meydana gelir. Bunun sebebi int'in hafızada işgal ettiği ünitesi sayısı kadar (4byte = 4 hafıza ünitesi) artış yapılır. 
+11. satırda diziAdres++ yapıldığında adres numarasında 1 artış gerçekleşmez. Bunun yerine dizinin bir sonraki elemanının adresine gidilir.
 12. satırda diziAdres'in gösterdiği adresteki veriyi yazmak için *diziAdres şeklinde kullanılmıştır. Ekran çıktısı 50'dir.40'dan 50'ye bir dizi elemanı kadar ilerlenmiştir.
 
 14. ve 15. satırda iki kere diziAdres-- yazılarak, iki dizi elemanı kadar geriye gidilmiştir, adres olarak 8 adım geriye gidilmiş oluyor.
